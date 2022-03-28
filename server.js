85d2775c42fdbba7c4e7ff74c0fee49369bae15c
Letton <admin@letton.tech>
2022-03-28 16:36:27 +0300

    Https fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 require("dotenv").config();
-const { createServer } = require("https");
+const http = require("http");
+const https = require("https");
 const { parse } = require("url");
 const sequelize = require("./db");
 const models = require("./models");
@@ -13,27 +14,39 @@ const port = process.env.PORT;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
-
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/letton.tech/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/letton.tech/cert.pem'),
-  ca: fs.readFileSync("/etc/letsencrypt/live/letton.tech/chain.pem")
-};
-
-
-
 (async () => {
   await sequelize.authenticate();
   await sequelize.sync();
   app.prepare().then(() => {
-    createServer(options, (req, res) => {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true);
-      handle(req, res, parsedUrl);
-    }).listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on http://${hostname}:${port}`);
-    });
+    if (process.env.NODE_ENV === "production") {
+      const options = {
+        key: fs.readFileSync("/etc/letsencrypt/live/letton.tech/privkey.pem"),
+        cert: fs.readFileSync("/etc/letsencrypt/live/letton.tech/cert.pem"),
+        ca: fs.readFileSync("/etc/letsencrypt/live/letton.tech/chain.pem"),
+      };
+      https
+        .createServer(options, (req, res) => {
+          // Be sure to pass `true` as the second argument to `url.parse`.
+          // This tells it to parse the query portion of the URL.
+          const parsedUrl = parse(req.url, true);
+          handle(req, res, parsedUrl);
+        })
+        .listen(port, (err) => {
+          if (err) throw err;
+          console.log(`> Ready on https://${hostname}:${port}`);
+        });
+    } else {
+      http
+        .createServer((req, res) => {
+          // Be sure to pass `true` as the second argument to `url.parse`.
+          // This tells it to parse the query portion of the URL.
+          const parsedUrl = parse(req.url, true);
+          handle(req, res, parsedUrl);
+        })
+        .listen(port, (err) => {
+          if (err) throw err;
+          console.log(`> Ready on http://${hostname}:${port}`);
+        });
+    }
   });
 })();
